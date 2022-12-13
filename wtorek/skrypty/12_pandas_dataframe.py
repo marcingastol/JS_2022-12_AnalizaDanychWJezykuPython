006873fa167bdc9c9fe568d1f32695edc4d9270b
Microsoft Learn Student <learn@contoso.com>
2022-12-13 15:54:46 +0100

    13.py

--- wtorek/skrypty/12_pandas_dataframe.py
+++ wtorek/skrypty/12_pandas_dataframe.py
@@ -8,15 +8,51 @@ a = {
 }
 
 b = pd.DataFrame(a, columns=["Key1","Key3","Key2","Key4"])
-print(b)
+#print(b)
 
 #print(b["Key3"])
 #print(b.Key3)
 
 #print(b.iloc[3])
 
-b["Key4"] = np.arange(8)
-print(b)
+#b["Key4"] = np.arange(8)
+#print(b)
 
 ##############################################################################
 
+c = pd.Series([1,2,3,4], index=["a","b","c","d"])
+
+b["Key5"] = c
+
+#print(b)
+
+del b["Key5"] 
+#print(b.columns)
+
+##############################################################################
+
+d = {
+    "Key1": {"Value1": 1,"Value2": 1,"Value3": 1,"Value4": 1,"Value5": 1},
+    "Key2": {"Value6": 1,"Value7": 1,"Value8": 1,"Value9": 1,"Value10": 1}
+}
+
+e = pd.DataFrame(d)
+
+#print(e.T)
+
+f = {
+    "Key1": e["Key1"][:-1],
+    "Key2": e["Key2"][1:6]
+}
+
+g = pd.DataFrame(f)
+
+#print(g)
+
+#print(g.to_numpy())
+
+################################################
+
+b = b.reindex(["a","b","c","d","e","f"])
+
+print(b)
---
+++ wtorek/skrypty/13_pandas_excercies.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+##################################################
+# 1. Porównaj elementy dwóch serii ze sobą : >, <, ==
+
+a = pd.Series([1,2,3,4,5])
+b = pd.Series([4,2,3,5,2])
+
